83bcf9b7ee0aa763957ec6b301d350b50b0717b0
cclion <crisps_13@163.com>
2018-12-04 18:43:50 +0800

    修改gitignore配置

---
+++ extend/cclion/Y.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace cclion;
+
+use think\Response;
+
+class Y
+{
+
+    //MARK: 封装接口返回格式
+    public static function json($code = 0, $message = '成功', $data){
+
+        $json = ['code' => $code, 'message' => $message, 'data' =>$data];
+        return Response::create($json, 'json', 200);
+    }
+
+}
