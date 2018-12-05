90fd886175c2ec630c5bbe86c830ed8bb4bd0adc
cclion <crisps_13@163.com>
2018-12-05 19:16:44 +0800

    封装返回api返回格式

--- extend/cclion/Y.php
+++ extend/cclion/Y.php
@@ -8,7 +8,7 @@ class Y
 {
 
     //MARK: 封装接口返回格式
-    public static function json($code = 0, $message = '成功', $data){
+    public static function json($code = 0, $message = '成功', $data = []){
 
         $json = ['code' => $code, 'message' => $message, 'data' =>$data];
         return Response::create($json, 'json', 200);
