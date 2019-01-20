3ad39be6f1787ab4c64a4bbea1109b6f28d97645
cclion <cclion0088@gmail.com>
2019-01-20 20:37:33 +0800

    添加Oauth逻辑

---
+++ application/index/controller/OAuth.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: job
+ * Date: 2019-01-17
+ * Time: 16:04
+ */
+namespace app\index\controller;
+
+use think\Controller;
+
+class OAuth extends Controller{
+
+
+
+
+}
