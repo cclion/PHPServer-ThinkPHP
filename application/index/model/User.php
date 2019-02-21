43a015df3633b34115affff89a4f9aa56b1314c3
cclion <cclion0088@gmail.com>
2019-02-21 19:11:40 +0800

    Add model

--- application/index/model/User.php
+++ application/index/model/User.php
@@ -11,15 +11,10 @@ use think\model;
 
 class User extends Model{
     // 设置当前模型对应的完整数据表名称
-    protected $table = 'User';
+    protected $table = 'user';
 
     // 设置当前模型的数据库连接
     protected $connection = 'db_config';
 
-    // 模型初始化
-    protected static function init()
-    {
-        //TODO:初始化内容
-    }
 
 }
